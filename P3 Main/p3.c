477e012b4a519a4ac99b51b1fa7cb14d321dc970
Stark Eon <44622096+StarkEon@users.noreply.github.com>
2018-10-31 10:47:06 +0100

    Implementada función leerDato

--- P3 Main/p3.c
+++ P3 Main/p3.c
@@ -246,11 +246,14 @@ int menu(void){
 /* Codificación completa de leerDato  */
 
 double leerDato (double min, double max){
-    int scan;
+
+    double scan;
     do{
         printf("Introduzca un valor real comprendido entre %.2f y %.2f", min, max);
-        scanf("%d", &scan);
-    }while(scan >= min && scan <= max);
+        scanf("%lf", &scan);
+    }while(scan <= min || scan >= max);
+
+    return(scan);
 
 }
 
