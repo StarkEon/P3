3edb0d1bd88b29c617668d509ed452660f73e070
Stark Eon <44622096+StarkEon@users.noreply.github.com>
2018-10-31 10:41:22 +0100

    Update Funciones/leerDato.c
    Removida función main

--- Funciones/leerDato.c
+++ Funciones/leerDato.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 
-double leerDato (double min, double max){
+double leerDato (double min, double max)
+{
     double scan;
     do{
         printf("Introduzca un valor real comprendido entre %.2f y %.2f", min, max);
@@ -9,13 +10,6 @@ double leerDato (double min, double max){
     }while(scan <= min || scan >= max);
 
     return(scan);
-
-}
-int main (void){
-    double d1 = 5.5;
-    double d2 = 150;
-    //double dato = leerDato(d1, d2);
-printf("%.2f", leerDato(d1, d2) );
 }
 
 
