53114413acf707beb088f107aa614eda04b1453a
Stark Eon <44622096+StarkEon@users.noreply.github.com>
2018-11-10 00:02:25 +0100

    //Ignorar
    Nueva carpeta para codificar Procesamiento_VMP

---
+++ Funciones/Procesamiento_VMP.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+#define VECES_PESO 4
+
+void pesosMes(double *c);
+
+int main (void)
+{
+    double col;
+    int i;
+
+    /*for(i=0;i<10;i++)
+    {
+        col[i] = i;
+    }*/
+
+    pesosMes(&col);
+    printf("%.2f",col);
+    return(0);
+}
+
+void pesosMes(VECES_PESO, double *pesos)
+{
+
+
+
+}
